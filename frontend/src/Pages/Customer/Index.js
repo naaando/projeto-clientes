5f12583c302c5215d88f2dbcdb730e42bd5ab0c8
Fernando da Silva Sousa <wild.nando@gmail.com>
2022-01-15 00:00:16 -0300

    Add pagination setup

--- frontend/src/Pages/Customer/Index.js
+++ frontend/src/Pages/Customer/Index.js
@@ -24,7 +24,53 @@ function openCustomer(id) {
   page(`/#/customer/${id}`)
 }
 
-function Customers({customers, error}) {
+function PaginationInfo({pagination}) {
+  // Generate 10 page numbers from -5 to +5 pages
+  const rangeGenerator = (n) => [n, n+1, n-1, n+2, n-2, n+3, n-3, n+4, n-4, n+5, n-5]
+    .filter(n => n > 0 && n < (1 + pagination.lastPage))
+    .slice(0, 5)
+    .sort((a, b) => a - b)
+  const pageRange = rangeGenerator(pagination.currentPage)
+
+  return (
+    <div className="flex my-2 items-center">
+      <div className='flex space-x-1'>
+        {pageRange.includes(1) || (
+          <a href="/#/customers?page=1" className='text-sm btn !px-2 block btn-blue'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+            </svg>
+          </a>
+        )}
+
+        {pageRange.map(x => x == pagination.currentPage ? (
+            <span key={x} className='text-sm btn bg-blue-800 text-white block select-none'>
+              {pagination.currentPage}
+            </span>
+          ) : (
+            <a key={x} href={`/#/customers?page=${x}`} className='text-sm btn btn-blue block'>
+              {x}
+            </a>
+          )
+        )}
+
+        {pageRange.includes(pagination.lastPage) || (
+          <a href={`/#/customers?page=${pagination.lastPage}`} className='text-sm btn !px-2 block btn-blue'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
+            </svg>
+          </a>
+        )}
+      </div>
+
+      <div className='text-gray-700 ml-auto'>
+        Exibindo de {pagination.from} a {pagination.to} de {pagination.total} clientes
+      </div>
+    </div>
+  )
+}
+
+function Customers({customers, pagination, error}) {
   // Request had an error
   if (error) {
     return errorMessage(error)
@@ -38,6 +84,8 @@ function Customers({customers, error}) {
   // Has customers
   return (
     <div className="p-2">
+      <PaginationInfo pagination={pagination} />
+
       <table className='bg-gray-100 rounded overflow-hidden w-full'>
         <thead>
           <tr className='bg-gray-800 text-white text-sm'>
@@ -71,21 +119,42 @@ function Customers({customers, error}) {
           ))}
         </tbody>
       </table>
+
+      <PaginationInfo pagination={pagination} />
     </div>
   )
 }
 
-export default function CustomerIndex() {
+export default function CustomerIndex(props) {
   const [error, setError] = useState(null);
   const [customers, setCustomers] = useState([]);
   const [search, setSearch] = useState('');
+  const [pagination, setPagination] = useState({});
+
+  const page = props.searchParams.get('page') || 1;
 
   useEffect(() => {
     client
-      .get('/customers', {params: {search}})
-      .then(response => setCustomers(response.data.data))
+      .get('/customers', {
+        params: {
+          search,
+          page,
+          perPage: 1
+        }
+      })
+      .then(response => {
+        setCustomers(response.data.data)
+        setPagination({
+          from: response.data['from'],
+          to: response.data['to'],
+          perPage: response.data['per_page'],
+          currentPage: response.data['current_page'],
+          lastPage: response.data['last_page'],
+          total: response.data['total'],
+        })
+      })
       .catch(setError)
-  }, [search]);
+  }, [search, page]);
 
   return (
     <div>
@@ -111,7 +180,7 @@ export default function CustomerIndex() {
           </a>
         </div>
 
-        <Customers customers={customers} error={error} />
+        <Customers customers={customers} pagination={pagination} error={error} />
       </div>
     </div>
   )
